41c0c2c44f56f03f3c3c3c3d36f64a60b9bee1b5
FE4902 <isdn000123@gmail.com>
2023-08-11 23:41:07 +0900

    Study: 글 삭제

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,9 +1,10 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Control() {
     const params = useParams();
+    const router = useRouter();
     const id = params.id;
 
     return (
@@ -17,7 +18,19 @@ export default function Control() {
                         <Link href={"/update/" + id}>Update</Link>
                     </li>
                     <li>
-                        <input type="button" value="delete" />
+                        <input
+                            type="button"
+                            value="delete"
+                            onClick={() => {
+                                const options = { method: "DELETE" };
+                                fetch(
+                                    "http://localhost:9999/topics/" + id,
+                                    options
+                                )
+                                    .then((resp) => resp.json())
+                                    .then((result) => router.push("/"));
+                            }}
+                        />
                     </li>
                 </>
             )}
